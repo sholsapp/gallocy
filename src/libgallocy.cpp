498b2cc62899947e062e9a68fed184c8a1d31104
Stephen Holsapple <sholsapp@gmail.com>
2012-06-20 11:32:41 -0700

    Call stubs working says printf.

--- src/libgallocy.cpp
+++ src/libgallocy.cpp
@@ -3,21 +3,31 @@
 
 
 class MmapHeap {
-  inline void* malloc(size_t sz) {
-    return NULL;
-  }
-  inline void free(void* ptr) {
-    return;
-  }
+  public:
+    inline void* malloc(size_t sz) {
+      fprintf(stderr, "> MmapHeap > malloc: Allocating %d byte(s)\n", sz);
+      return NULL;
+    }
+    inline void free(void* ptr) {
+      fprintf(stderr, "> MmapHeap > free: Freeing %p\n", ptr);
+      return;
+    }
+  private:
+    void* zone;
 };
 
 class MainHeap : public MmapHeap {};
 
+MainHeap heap;
+
 extern "C" {
-  void* custom_malloc(size_t) {
-    return NULL;
+  void* custom_malloc(size_t sz) {
+    fprintf(stderr, "> custom_malloc: Allocating %d byte(s)\n", sz);
+    return heap.malloc(sz);
   }
   void custom_free(void* ptr) {
+    fprintf(stderr, "> custom_free: Freeing %p\n", ptr);
+    heap.free(ptr);
     return;
   }
 }
