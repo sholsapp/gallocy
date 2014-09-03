765661146de024be3db157a194eca08fe3692185
Stephen Holsapple <sholsapp@gmail.com>
2014-09-02 20:13:06 -0700

    Added death test.
    But kept it disabled for now because it slows down test suite.

--- test/gtest.cpp
+++ test/gtest.cpp
@@ -1,6 +1,13 @@
 #include "gtest/gtest.h"
 
 
-TEST(GallocyTest, AssertionTrue) {
-    ASSERT_EQ(1, 1);
+TEST(SanityTest, AssertionTrue) {
+    ASSERT_EQ(1, 1) << "Sanity check failed.";
 }
+
+#if 0
+TEST(SanityTest, Death) {
+  int *i = NULL;
+  ASSERT_DEATH({ *((int *) i) = 0; }, "");
+}
+#endif
