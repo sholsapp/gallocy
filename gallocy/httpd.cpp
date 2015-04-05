73f081d0578c4a3e81f373162f16ccff8b750c08
Stephen Holsapple <sholsapp@gmail.com>
2015-04-05 10:23:28 -0700

    Refactored httpd.c to include only library code
    Leave main execution to the binary HTTPD module

--- gallocy/httpd.cpp
+++ gallocy/httpd.cpp
@@ -1,48 +1,5 @@
-/* J. David's webserver */
-/* This is a simple webserver.
- * Created November 1999 by J. David Blackstone.
- * CSE 4344 (Network concepts), Prof. Zeigler
- * University of Texas at Arlington
- */
-/* This program compiles for Sparc Solaris 2.6.
- * To compile for Linux:
- *  1) Comment out the #include <pthread.h> line.
- *  2) Comment out the line that defines the variable newthread.
- *  3) Comment out the two lines that run pthread_create().
- *  4) Uncomment the line that runs accept_request().
- *  5) Remove -lsocket from the Makefile.
- */
-#include <stdio.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <ctype.h>
-#include <strings.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <pthread.h>
-#include <sys/wait.h>
-#include <stdlib.h>
+#include "httpd.h"
 
-#define ISspace(x) isspace((int)(x))
-
-#define SERVER_STRING "Server: jdbhttpd/0.1.0\r\n"
-
-void *accept_request(void *);
-void bad_request(int);
-void cat(int, FILE *);
-void cannot_execute(int);
-void error_die(const char *);
-void execute_cgi(int, const char *, const char *, const char *);
-int get_line(int, char *, int);
-void headers(int, const char *);
-void add_header(int, const char *, const char *);
-void not_found(int);
-void serve_file(int, const char *);
-int startup(u_short *);
-void unimplemented(int);
 
 /**********************************************************************/
 /* A request has caused a call to accept() on the server port to
@@ -488,35 +445,4 @@ void unimplemented(int client)
   send(client, buf, strlen(buf), 0);
   sprintf(buf, "</BODY></HTML>\r\n");
   send(client, buf, strlen(buf), 0);
-}
-
-/**********************************************************************/
-
-int main(void)
-{
-  int server_sock = -1;
-  u_short port = 8080;
-  long client_sock = -1;
-  struct sockaddr_in client_name;
-  unsigned int client_name_len = sizeof(client_name);
-  pthread_t newthread;
-
-  server_sock = startup(&port);
-  printf("httpd running on port %d\n", port);
-
-  while (1)
-  {
-    client_sock = accept(server_sock,
-        (struct sockaddr *)&client_name,
-        &client_name_len);
-    if (client_sock == -1)
-      error_die("accept");
-    /* accept_request(client_sock); */
-    if (pthread_create(&newthread , NULL, accept_request, (void *) client_sock) != 0)
-      perror("pthread_create");
-  }
-
-  close(server_sock);
-
-  return(0);
 }
