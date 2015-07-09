c97a133e387f7514dbf47163918846c95d4837a3
Stephen Holsapple <sholsapp@gmail.com>
2015-07-08 23:40:32 -0700

    Add ifndef/define protections around header file.

--- gallocy/config.h
+++ gallocy/config.h
@@ -1,3 +1,6 @@
+#ifndef _CONFIG_H
+#define _CONFIG_H
+
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
@@ -12,9 +15,10 @@
 
 typedef gallocy::vector<gallocy::string> peer_list_t;
 
-typedef gallocy::map<int, int> foo;
-
 
 gallocy::string read_file(const char*);
+void read_config(gallocy::string &, peer_list_t &);
 gallocy::string get_me(void);
 peer_list_t get_peers(void);
+
+#endif
