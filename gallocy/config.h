4d38500aaed68e1a72bfce52d9799f4db22b2814
Stephen Holsapple <sholsapp@gmail.com>
2015-07-08 22:22:45 -0700

    Deleted STLHeap... it doesn't appear to be used
    This is a scary change. I hope the testing didn't miss anything.
    Warnings in the code I deleted explain it all.

--- gallocy/config.h
+++ gallocy/config.h
@@ -10,21 +10,6 @@
 #include "libgallocy.h"
 
 
-// XXX: I'm not sure why instantiating the class like this and passing it to
-// (viz.) the gallocy::string type works, but passing the SingletonHeapType
-// class directly does not.
-class STLHeap :
-  // XXX: Move anyThreadsCreated from
-  // libgallocy.cpp into a stand alone module that
-  // both gallocy-core and gallocy-net can depend
-  // on. If we move this into SingletonHeapType
-  // then everyone needs to depend on gallocy-core
-  // for the single variable.
-  public HL::LockedHeap<
-    HL::SpinLockType,
-    HL::SingletonHeap > {};
-
-
 typedef gallocy::vector<gallocy::string> peer_list_t;
 
 typedef gallocy::map<int, int> foo;
