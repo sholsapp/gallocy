2940a3f9486feeedadd33514d1f8b29e14be1094
Stephen Holsapple <sholsapp@gmail.com>
2015-07-15 10:14:44 -0700

    Add a run script to packaged distributions.

--- tools/make_package.py
+++ tools/make_package.py
@@ -7,6 +7,8 @@ import contextlib
 import os
 import platform
 import tarfile
+import tempfile
+import shutil
 
 
 @contextlib.contextmanager
@@ -20,12 +22,24 @@ def chdir(dirname=None):
     os.chdir(curdir)
 
 
+@contextlib.contextmanager
+def mkdtmp(suffix='', prefix='', dir=None):
+  tmp = None
+  try:
+    tmp = tempfile.mkdtemp(suffix=suffix, prefix=prefix, dir=dir)
+    yield tmp
+  finally:
+    if tmp:
+      shutil.rmtree(tmp)
+
+
 def package_name(version):
   return 'gallocy-{0}-{1}-{2}.tar.gz'.format(
     version,
     platform.system().lower(),
     platform.machine().lower(),
   )
+
 
 def main():
 
@@ -35,10 +49,26 @@ def main():
 
   tar = tarfile.open(package_name(version), 'w:gz')
 
-  with chdir('install'):
-    tar.add('bin')
-    tar.add('lib')
-    tar.close()
+  gallocy_root = os.getcwd()
+
+  with mkdtmp() as tmp:
+
+    shutil.copytree(os.path.join(gallocy_root, 'install', 'bin'), os.path.join(tmp, 'bin'))
+    shutil.copytree(os.path.join(gallocy_root, 'install', 'lib'), os.path.join(tmp, 'lib'))
+
+    with open(os.path.join(tmp, 'run'), 'w') as fh:
+      fh.writelines([
+        '#!/bin/bash\n',
+        'LD_LIBRARY_PATH=lib GALLOCY_CONFIG=config.json ./bin/httpd\n'
+      ])
+      os.fchmod(fh.fileno(), 0755)
+
+    with chdir(tmp):
+      tar.add('bin')
+      tar.add('lib')
+      tar.add('run')
+
+  tar.close()
 
 if __name__ == '__main__':
   main()
