8cf4458aa0e85264b15418ef5f1ea7932ff5b3f0
Stephen Holsapple <sholsapp@gmail.com>
2015-11-03 21:00:14 -0800

    Fixes for C++11, Clang and googletest.
    There are a few trivial compilation errors in googletest's macros when
    using C++11 and Clang. This change fixes these errors.

--- test/test_json.cpp
+++ test/test_json.cpp
@@ -16,21 +16,21 @@ TEST(JsonTests, BasicTest) {
   j["list"] = { "foo", "bar", "baz" };
   j["object"] = { { "sub-int", 1 } };
   ASSERT_EQ(j["string"], "abc");
-  ASSERT_EQ(j["int"], 1);
-  ASSERT_EQ(j["float"], 3.141);
-  ASSERT_EQ(j["bool"], true);
+  ASSERT_EQ(j["int"], gallocy::json(1));
+  ASSERT_EQ(j["float"], gallocy::json(3.141));
+  ASSERT_EQ(j["bool"], gallocy::json(true));
   ASSERT_EQ(j["list"].size(), 3);
   ASSERT_EQ(j["list"][0], "foo");
   ASSERT_EQ(j["list"][1], "bar");
   ASSERT_EQ(j["list"][2], "baz");
   ASSERT_EQ(j["list"][3], gallocy::json(nullptr));
-  ASSERT_EQ(j["object"]["sub-int"], 1);
+  ASSERT_EQ(j["object"]["sub-int"], gallocy::json(1));
 }
 
 TEST(JsonTests, ParseTest) {
   gallocy::json j = gallocy::json::parse("{\"happy\":true,\"list\":[\"a\",\"b\"],\"pi\":3.141}");
-  ASSERT_EQ(j["happy"], true);
-  ASSERT_EQ(j["pi"], 3.141);
+  ASSERT_EQ(j["happy"], gallocy::json(true));
+  ASSERT_EQ(j["pi"], gallocy::json(3.141));
   ASSERT_EQ(j["list"].size(), 2);
   ASSERT_EQ(j["list"][0], "a");
   ASSERT_EQ(j["list"][1], "b");
