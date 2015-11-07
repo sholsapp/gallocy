b752f3c54ec54acd7e3c5515f2292ee70a469d53
Stephen Holsapple <sholsapp@gmail.com>
2015-11-06 21:00:54 -0500

    styleguide
    This commit feels a little questionable due to some casting precendence
    issues.

--- gallocy/heaplayers/sizeheap.h
+++ gallocy/heaplayers/sizeheap.h
@@ -1,106 +1,51 @@
-#ifndef _SIZEHEAP_H_
-#define _SIZEHEAP_H_
-
-/**
- * @file sizeheap.h
- * @brief Contains UseSizeHeap and SizeHeap.
- */
-
-#include <assert.h>
+#ifndef GALLOCY_HEAPLAYERS_SIZEHEAP_H_
+#define GALLOCY_HEAPLAYERS_SIZEHEAP_H_
 
 #include "addheap.h"
 
-#if 1
-
-/**
- * @class UseSizeHeap
- * @brief Adds a getSize method to access the size of an allocated object.
- * @see SizeHeap
- */
 
 namespace HL {
 
 template <class Super>
 class UseSizeHeap : public Super {
-public:
+ public:
+  inline UseSizeHeap(void) {}
 
-  inline UseSizeHeap (void) {}
-
-  inline static size_t getSize (const void * ptr) {
-    return ((freeObject *) ptr - 1)->sz;
+  inline static size_t getSize(void *ptr) {
+    return (reinterpret_cast<freeObject *>(ptr) - 1)->sz;
   }
 
-protected:
+ protected:
   union freeObject {
     size_t sz;
-    double _dummy; // for alignment.
+    double _dummy;  // for alignment.
   };
 };
 
-/**
- * @class SizeHeap
- * @brief Allocates extra room for the size of an object.
- */
 
 template <class SuperHeap>
 class SizeHeap : public UseSizeHeap<SuperHeap> {
   typedef typename UseSizeHeap<SuperHeap>::freeObject freeObject;
-public:
-  inline SizeHeap (void) {}
-  inline void * malloc (const size_t sz) {
+ public:
+  inline SizeHeap(void) {}
+
+  inline void *malloc(const size_t sz) {
     // Add room for a size field.
-    freeObject * ptr = (freeObject *)
-      SuperHeap::malloc (sz + sizeof(freeObject));
+    freeObject *ptr = reinterpret_cast<freeObject *>(SuperHeap::malloc(sz + sizeof(freeObject)));
     // Store the requested size.
     ptr->sz = sz;
-    return (void *) (ptr + 1);
+    return reinterpret_cast<void *>(ptr + 1);
   }
-  inline void free (void * ptr) {
-    SuperHeap::free ((freeObject *) ptr - 1);
+
+  inline void free(void *ptr) {
+    SuperHeap::free(reinterpret_cast<freeObject *>(ptr) - 1);
   }
+
   inline void __reset() {
     SuperHeap::__reset();
   }
 };
 
-};
+}  // namespace HL
 
-#else
-
-template <class Super>
-class SizeHeap : public Super {
-public:
-
-  inline void * malloc (size_t sz) {
-    // Add room for a size field.
-	  assert (sizeof(size_t) <= sizeof(double));
-    void * ptr = Super::malloc (sz + sizeof(double));
-    // Store the requested size.
-    *((size_t *) ptr) = sz;
-    return (void *) ((double *) ptr + 1);
-  }
-
-  inline void free (void * ptr) {
-    void * origPtr = (void *) ((double *) ptr - 1);
-	Super::free (origPtr);
-  }
-
-  inline static size_t getSize (void * ptr) {
-    return *((size_t *) ((double *) ptr - 1));
-  }
-};
-
-
-
-template <class Super>
-class UseSizeHeap : public Super {
-public:
-
-  inline static size_t getSize (void * ptr) {
-    return *((size_t *) ((double *) ptr - 1));
-  }
-};
-
-#endif
-
-#endif
+#endif  // GALLOCY_HEAPLAYERS_SIZEHEAP_H_
