ffb15b2223c4c6a23ce4224eeeac1db22e65d886
Stephen Holsapple <sholsapp@gmail.com>
2015-11-06 20:52:40 -0500

    styleguide

--- gallocy/heaplayers/hash.h
+++ gallocy/heaplayers/hash.h
@@ -1,26 +1,26 @@
-#ifndef _HL_HASH_H_
-#define _HL_HASH_H_
+#ifndef GALLOCY_HEAPLAYERS_HASH_H_
+#define GALLOCY_HEAPLAYERS_HASH_H_
 
 #include <cstdlib>
 
 namespace HL {
   template <typename Key>
-    extern size_t hash (Key k);
+    extern size_t hash(Key k);
 
   template <>
-    inline size_t hash (void * v) {
+    inline size_t hash(void *v) {
     return (size_t) v;
   }
 
   template <>
-    inline size_t hash (const void * v) {
+    inline size_t hash(const void *v) {
     return (size_t) ((size_t) v);
   }
 
   template <>
-    inline size_t hash (int v) {
+    inline size_t hash(int v) {
     return (size_t) v;
   }
-}
+}  // namespace HL
 
-#endif
+#endif  // GALLOCY_HEAPLAYERS_HASH_H_
