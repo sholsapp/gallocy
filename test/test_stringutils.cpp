d1ebd0c3e96ca4922ae58bd68c31fb45d989cf5a
Stephen Holsapple <sholsapp@gmail.com>
2015-11-27 09:14:19 -0500

    Add more tests for string utilities
    Hunting for a memory leak.

--- test/test_stringutils.cpp
+++ test/test_stringutils.cpp
@@ -13,3 +13,23 @@ TEST(StringutilsTests, EndsWith) {
   ASSERT_TRUE(utils::endswith("xxxaaabbbccc", "ccc"));
   ASSERT_FALSE(utils::endswith("xxxaaabbbccc", "bbb"));
 }
+
+
+TEST(StringutilsTests, Split) {
+  gallocy::vector<gallocy::string> v;
+  gallocy::string t = "a b c d";
+  utils::split(t, ' ', v);
+  ASSERT_EQ(v.size(), static_cast<size_t>(4));
+}
+
+
+TEST(StringutilsTests, Trim) {
+  gallocy::vector<gallocy::string> v;
+  gallocy::string t = "a b c d";
+  utils::split(t, ' ', v);
+  ASSERT_EQ(v.size(), static_cast<size_t>(4));
+  ASSERT_EQ(utils::trim(v[0]), "a");
+  ASSERT_EQ(utils::trim(v[1]), "b");
+  ASSERT_EQ(utils::trim(v[2]), "c");
+  ASSERT_EQ(utils::trim(v[3]), "d");
+}
