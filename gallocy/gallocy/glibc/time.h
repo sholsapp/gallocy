20ec9d49cebd644c7744917c60973a0b8f5e067d
Stephen Holsapple <sholsapp@gmail.com>
2016-01-21 03:58:21 -0500

    Add a glibc module.
    This module seeks to wrap or reimplement glibc functionality that
    internally uses malloc. This is a necessary evil (as far as I can tell)
    to allow us to use glibc while at the same not prevent polluting the
    application's address space.

---
+++ gallocy/gallocy/glibc/time.h
@@ -0,0 +1,3 @@
+#include <time.h>
+
+struct tm *gmtime_r(const time_t *timer, struct tm *tmbuf);
---
+++ gallocy/glibc/time.c
@@ -0,0 +1,48 @@
+#include <time.h>
+
+#include "gallocy/glibc/time.h"
+
+#define YEAR0                   1900
+#define EPOCH_YR                1970
+#define SECS_DAY                (24L * 60L * 60L)
+#define LEAPYEAR(year)          (!((year) % 4) && (((year) % 100) || !((year) % 400)))
+#define YEARSIZE(year)          (LEAPYEAR(year) ? 366 : 365)
+#define FIRSTSUNDAY(timp)       (((timp)->tm_yday - (timp)->tm_wday + 420) % 7)
+#define FIRSTDAYOF(timp)        (((timp)->tm_wday - (timp)->tm_yday + 420) % 7)
+
+const int _ytab[2][12] = {
+  {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+  {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+};
+
+struct tm *gmtime_r(const time_t *timer, struct tm *tmbuf) {
+
+  time_t time = *timer;
+  unsigned long dayclock, dayno;
+  int year = EPOCH_YR;
+
+  dayclock = (unsigned long) time % SECS_DAY;
+  dayno = (unsigned long) time / SECS_DAY;
+
+  tmbuf->tm_sec = dayclock % 60;
+  tmbuf->tm_min = (dayclock % 3600) / 60;
+  tmbuf->tm_hour = dayclock / 3600;
+  tmbuf->tm_wday = (dayno + 4) % 7; // Day 0 was a thursday
+  while (dayno >= (unsigned long) YEARSIZE(year)) {
+    dayno -= YEARSIZE(year);
+    year++;
+  }
+  tmbuf->tm_year = year - YEAR0;
+  tmbuf->tm_yday = dayno;
+  tmbuf->tm_mon = 0;
+  while (dayno >= (unsigned long) _ytab[LEAPYEAR(year)][tmbuf->tm_mon]) {
+    dayno -= _ytab[LEAPYEAR(year)][tmbuf->tm_mon];
+    tmbuf->tm_mon++;
+  }
+  tmbuf->tm_mday = dayno + 1;
+  tmbuf->tm_isdst = 0;
+  tmbuf->tm_gmtoff = 0;
+  tmbuf->tm_zone = "UTC";
+
+  return tmbuf;
+}
