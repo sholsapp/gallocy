ddf8bfa213043f83de95aba113dfc238dc847f1d
Stephen Holsapple <sholsapp@gmail.com>
2016-02-04 09:22:55 -0500

    Break out callback functions as top level functions.
    Break out the callback functions so that we can provide overloaded
    definitions of methods like, append entries.

--- gallocy/consensus/client.cpp
+++ gallocy/consensus/client.cpp
@@ -1,28 +1,33 @@
+#include <vector>
+
 #include "gallocy/consensus/client.h"
+#include "gallocy/consensus/log.h"
 #include "gallocy/entrypoint.h"
 #include "gallocy/utils/http.h"
 #include "restclient.h"  // NOLINT
+
+
+std::function<bool(const RestClient::Response &)> request_vote_callback = [](const RestClient::Response &rsp) {
+  if (rsp.code == 200) {
+    gallocy::string body = rsp.body.c_str();
+    gallocy::json response_json = gallocy::json::parse(body);
+    bool granted = response_json["vote_granted"];
+    uint64_t supporter_term = response_json["term"];
+    uint64_t local_term = gallocy_state->get_current_term();
+    if (supporter_term > local_term) {
+      gallocy_state->set_state(RaftState::FOLLOWER);
+      gallocy_state->set_current_term(supporter_term);
+    }
+    return granted;
+  }
+  return false;
+};
 
 
 bool GallocyClient::send_request_vote() {
   uint64_t candidate_term = gallocy_state->get_current_term();
   uint64_t candidate_last_applied = gallocy_state->get_last_applied();
   uint64_t candidate_commit_index = gallocy_state->get_commit_index();
-  std::function<bool(const RestClient::Response &)> callback = [](const RestClient::Response &rsp) {
-    if (rsp.code == 200) {
-      gallocy::string body = rsp.body.c_str();
-      gallocy::json response_json = gallocy::json::parse(body);
-      bool granted = response_json["vote_granted"];
-      uint64_t supporter_term = response_json["term"];
-      uint64_t local_term = gallocy_state->get_current_term();
-      if (supporter_term > local_term) {
-        gallocy_state->set_state(RaftState::FOLLOWER);
-        gallocy_state->set_current_term(supporter_term);
-      }
-      return granted;
-    }
-    return false;
-  };
 
   gallocy::json j = {
     { "term", candidate_term },
@@ -32,10 +37,29 @@ bool GallocyClient::send_request_vote() {
 
   // TODO(sholsapp): How we handle this is busted and needs to be refactored so
   // that the cv is usable here. This is also blocking, which is probably bad?
-  uint64_t votes = utils::post_many("/raft/request_vote", config.peers, config.port, j.dump(), callback);
+  uint64_t votes = utils::post_many("/raft/request_vote", config.peers, config.port,
+                                    j.dump(), request_vote_callback);
+
   LOG_INFO("Received votes from " << votes << "/" << config.peers.size() << " peers");
   return votes >= config.peers.size() / 2;
 }
+
+
+std::function<bool(const RestClient::Response &)> append_entries_callback = [](const RestClient::Response &rsp) {
+  if (rsp.code == 200) {
+    gallocy::string body = rsp.body.c_str();
+    gallocy::json response_json = gallocy::json::parse(body);
+    bool success = response_json["success"];
+    uint64_t supporter_term = response_json["term"];
+    uint64_t local_term = gallocy_state->get_current_term();
+    if (supporter_term > local_term) {
+      gallocy_state->set_state(RaftState::FOLLOWER);
+      gallocy_state->set_current_term(supporter_term);
+    }
+    return success;
+  }
+  return false;
+};
 
 
 bool GallocyClient::send_append_entries() {
@@ -45,23 +69,8 @@ bool GallocyClient::send_append_entries() {
   uint64_t leader_prev_log_index = gallocy_state->get_log()->get_previous_log_index();
   uint64_t leader_prev_log_term = gallocy_state->get_log()->get_previous_log_term();
 
-  std::function<bool(const RestClient::Response &)> callback = [](const RestClient::Response &rsp) {
-    if (rsp.code == 200) {
-      gallocy::string body = rsp.body.c_str();
-      gallocy::json response_json = gallocy::json::parse(body);
-      bool success = response_json["success"];
-      uint64_t supporter_term = response_json["term"];
-      uint64_t local_term = gallocy_state->get_current_term();
-      if (supporter_term > local_term) {
-        gallocy_state->set_state(RaftState::FOLLOWER);
-        gallocy_state->set_current_term(supporter_term);
-      }
-      return success;
-    }
-    return false;
-  };
-
   gallocy::json j = {
+    // A heartbeat request is an "append entries" with no entries.
     { "entries", gallocy::json::array() },
     { "leader_commit", leader_commit_index },
     { "previous_log_index", leader_prev_log_index },
@@ -71,7 +80,36 @@ bool GallocyClient::send_append_entries() {
 
   // TODO(sholsapp): How we handle this is busted and needs to be refactored so
   // that the cv is usable here. This is also blocking, which is probably bad?
-  uint64_t votes = utils::post_many("/raft/append_entries", config.peers, config.port, j.dump(), callback);
+  uint64_t votes = utils::post_many("/raft/append_entries", config.peers, config.port,
+                                    j.dump(), append_entries_callback);
+
+  return true;
+}
+
+
+bool GallocyClient::send_append_entries(const gallocy::vector<LogEntry> &entries) {
+  uint64_t leader_term = gallocy_state->get_current_term();
+  uint64_t leader_last_applied = gallocy_state->get_last_applied();
+  uint64_t leader_commit_index = gallocy_state->get_commit_index();
+  uint64_t leader_prev_log_index = gallocy_state->get_log()->get_previous_log_index();
+  uint64_t leader_prev_log_term = gallocy_state->get_log()->get_previous_log_term();
+
+  gallocy::json j = {
+    { "entries", gallocy::json::array() },
+    { "leader_commit", leader_commit_index },
+    { "previous_log_index", leader_prev_log_index },
+    { "previous_log_term", leader_prev_log_term },
+    { "term", leader_term },
+  };
+
+  for (auto &entry : entries) {
+    j["entries"].push_back(entry.to_json());
+  }
+
+  // TODO(sholsapp): How we handle this is busted and needs to be refactored so
+  // that the cv is usable here. This is also blocking, which is probably bad?
+  uint64_t votes = utils::post_many("/raft/append_entries", config.peers, config.port,
+                                    j.dump(), append_entries_callback);
 
   return true;
 }
--- gallocy/include/gallocy/consensus/client.h
+++ gallocy/include/gallocy/consensus/client.h
@@ -1,6 +1,9 @@
 #ifndef GALLOCY_CONSENSUS_CLIENT_H_
 #define GALLOCY_CONSENSUS_CLIENT_H_
 
+#include <vector>
+
+#include "gallocy/consensus/log.h"
 #include "gallocy/utils/config.h"
 
 class GallocyClient {
@@ -12,20 +15,36 @@ class GallocyClient {
    * Make a "request vote" request.
    *
    * This method makes many requests internally, which is required to make a
-   * single "request vote" request. This is the nature of consensus.
+   * single "request vote" request.
    *
    * \return True if the success criteria is met.
    */
   bool send_request_vote();
   /**
-   * Make a "append entries" request.
+   * Make an "append entries" request.
+   *
+   * This method is useful for creating "heart beat" messages which, per the
+   * Raft protocol, is an "append entries" request with no entries. For
+   * actually appending entries to the log, see \ref
+   * GallocyClient::send_append_entries(const gallocy::vector<Command> &entries).
    *
    * This method makes many requests internally, which is required to make a
-   * single "append entries" request. This is the nature of consensus.
+   * single "append entries" request.
    *
    * \return True if the success criteria is met.
    */
   bool send_append_entries();
+  /**
+   * Make an "append entries" request.
+   *
+   * This method is useful for creating messages which append entries to the
+   * log and should not be used for "heart beat" messages. See \ref
+   * GallocyClient::send_append_entries() for sending "heart beat" messages.
+   *
+   * This method makes many requests internally, which is required to make a
+   * single "append entries" request.
+   */
+  bool send_append_entries(const gallocy::vector<LogEntry> &entries);
 
  private:
   GallocyConfig &config;
