5726bfb3d8db83e873654288336f00929d239858
Stephen Holsapple <sholsapp@gmail.com>
2016-02-04 23:35:30 -0500

    Implement a peer abstraction.
    This should be the complete implementation that we need, but I haven't
    refactored the rest of the code to use it quite yet. I will do this
    next, as well as write tests.
    
    https://github.com/sholsapp/gallocy/issues/29

---
+++ gallocy/include/gallocy/peer.h
@@ -0,0 +1,101 @@
+#ifndef GALLOCY_PEER_H_
+#define GALLOCY_PEER_H_
+
+#include <functional>
+
+#include "gallocy/allocators/internal.h"
+
+/**
+ * A peer abstraction.
+ *
+ * This abstraction should normalize how we deal with peers everywhere in the
+ * code. It is a common code that should be used within the consensus and http
+ * modules, but should not maintain state specific to specialized use cases.
+ */
+class Peer {
+ public:
+  /**
+   * Create a peer.
+   *
+   * \param internet_address The dot notation internet address string.
+   * \param port The port.
+   */
+  Peer(const gallocy::string &internet_address, uint16_t port) {
+    internet_address_integer = parse_internet_address(internet_address);
+    port_integer = port;
+  }
+  /**
+   * Create a peer.
+   *
+   * \param internet_address The integer representation of the internet address.
+   * \param port The port.
+   */
+  Peer(uint64_t internet_address, uint16_t port) {
+    internet_address_integer = internet_address;
+    port = port;
+  }
+  /**
+   * Get the peer's internet address as a string in dot notation.
+   *
+   * \warning Always prefer \ref Peer::get_canonical_id when implementing
+   * internal logic that depends on the peer's identity. Use this method for
+   * logging only.
+   *
+   * \return The internet address string.
+   */
+  const gallocy::string get_string() const;
+  /**
+   * Get the peer's canonical identifier.
+   *
+   * \return The peer's canonical identifier.
+   */
+  const uint64_t get_canonical_id() const;
+
+ private:
+  /**
+   * Unparse an IPv4 internet address from an integer.
+   *
+   *  \warning This function, like its counterpart, naively unparses an IPv4
+   *  internet address into a string. This function needs to be rewritten such
+   *  that endianness and IPv6 is handled.
+   *
+   * \param The IPv4 internet address as an integer.
+   * \return A dot-notation string of the internet address.
+   */
+  const gallocy::string unparse_internet_address(uint64_t ip_address) const;
+  /**
+   * Parse an IPv4 internet address from a string.
+   *
+   *  \warning This function naively parses an IPv4 internet address from a
+   *  string.  This function needs to be rewritten such that endian-ness and
+   *  IPv6 is handled.
+   *
+   * \param ip_address The IPv4 internet address as a string.
+   * \return An unsigned 64 bit integer of the IPv4 address.
+   */
+  const uint64_t parse_internet_address(const gallocy::string &ip_address) const;
+  /**
+   * Used as the peer's canonical identifier.
+   */
+  uint64_t internet_address_integer;
+  /**
+   * The port.
+   */
+  uint16_t port_integer;
+};
+
+
+namespace std {
+
+// COMPLETE specialization allows the Peer type to be used as a key in map and
+// set objects.
+template<> struct less<Peer> {
+  bool operator() (const Peer &lhs, const Peer &rhs) const {
+    return lhs.get_canonical_id() < rhs.get_canonical_id();
+  }
+};
+
+}  // namespace std
+
+
+#endif  // GALLOCY_PEER_H_
---
+++ gallocy/peer.cpp
@@ -0,0 +1,41 @@
+#include <arpa/inet.h>
+
+#include "gallocy/allocators/internal.h"
+#include "gallocy/peer.h"
+
+
+const gallocy::string Peer::get_string() const {
+  return unparse_internet_address(internet_address_integer);
+}
+
+
+const uint64_t Peer::get_canonical_id() const {
+  // TODO(sholsapp): When we wish to run two peers on the same machine, we'll
+  // need to adjust this so that we include the port as well.
+  return internet_address_integer;
+}
+
+
+const uint64_t Peer::parse_internet_address(const gallocy::string &ip_address) const {
+  uint64_t ip = 0;
+  uint8_t buf[4];
+  int r = inet_pton(AF_INET, ip_address.c_str(), buf);
+  if (r <= 0) {
+    return 0;
+  }
+  for (int i = 0; i < 4; i++) {
+    ip <<= 8;
+    ip |= buf[i] & 0xff;
+  }
+  return ip;
+}
+
+
+const gallocy::string Peer::unparse_internet_address(uint64_t ip_address) const {
+  // HACK off half of the uint64_t representation after converting it to
+  // network order.
+  uint32_t half_of_long = static_cast<uint32_t>(htonl(ip_address));
+  struct in_addr ip_addr;
+  ip_addr.s_addr = half_of_long;
+  return inet_ntoa(ip_addr);
+}
