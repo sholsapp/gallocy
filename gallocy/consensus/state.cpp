b99d85a1c40c5e3d5e3d179e2c5463b80378c1c4
Stephen Holsapple <sholsapp@gmail.com>
2016-02-29 00:10:31 -0500

    Move GallocyState's implementation into compilation unit.

--- gallocy/consensus/state.cpp
+++ gallocy/consensus/state.cpp
@@ -1,14 +1,148 @@
 #include "gallocy/allocators/internal.h"
+#include "gallocy/consensus/log.h"
 #include "gallocy/consensus/state.h"
+#include "gallocy/consensus/timer.h"
 
 
 const gallocy::string raft_state_to_string(RaftState state) {
-  if (state == RaftState::FOLLOWER)
-    return "FOLLOWER";
-  else if (state == RaftState::LEADER)
-    return "LEADER";
-  else if (state == RaftState::CANDIDATE)
-    return "CANDIDATE";
-  else
-    abort();
+    if (state == RaftState::FOLLOWER)
+        return "FOLLOWER";
+    else if (state == RaftState::LEADER)
+        return "LEADER";
+    else if (state == RaftState::CANDIDATE)
+        return "CANDIDATE";
+    else
+        abort();
+}
+
+
+uint64_t GallocyState::get_current_term() {
+    std::lock_guard<std::mutex> lock(access_lock);
+    return current_term;
+}
+
+
+void GallocyState::set_current_term(uint64_t value) {
+    std::lock_guard<std::mutex> lock(access_lock);
+    if (value < current_term)
+        LOG_ERROR("Regressing current term from "
+                << current_term
+                << " to "
+                << value
+                << ". This is a logic error.");
+    current_term = value;
+    voted_for = 0;
+}
+
+
+PeerId GallocyState::get_voted_for() {
+    std::lock_guard<std::mutex> lock(access_lock);
+    return voted_for;
+}
+
+
+void GallocyState::set_voted_for(PeerId value) {
+    std::lock_guard<std::mutex> lock(access_lock);
+    voted_for = value;
+}
+
+
+uint64_t GallocyState::get_commit_index() {
+    std::lock_guard<std::mutex> lock(access_lock);
+    return commit_index;
+}
+
+
+void GallocyState::set_commit_index(uint64_t value) {
+    std::lock_guard<std::mutex> lock(access_lock);
+    commit_index = value;
+}
+
+
+uint64_t GallocyState::get_last_applied() {
+    std::lock_guard<std::mutex> lock(access_lock);
+    return last_applied;
+}
+
+
+void GallocyState::set_last_applied(uint64_t value) {
+    std::lock_guard<std::mutex> lock(access_lock);
+    last_applied = value;
+}
+
+
+uint64_t GallocyState::get_next_index(PeerId peer) {
+    std::lock_guard<std::mutex> lock(access_lock);
+    return next_index[peer];
+}
+
+
+void GallocyState::set_next_index(PeerId peer, uint64_t value) {
+    std::lock_guard<std::mutex> lock(access_lock);
+    next_index[peer] = value;
+}
+
+
+uint64_t GallocyState::get_match_index(PeerId peer) {
+    std::lock_guard<std::mutex> lock(access_lock);
+    return match_index[peer];
+}
+
+
+void GallocyState::set_match_index(PeerId peer, uint64_t value) {
+    std::lock_guard<std::mutex> lock(access_lock);
+    match_index[peer] = value;
+}
+
+GallocyLog *GallocyState::get_log() {
+    return log;
+}
+
+
+Timer *GallocyState::get_timer() {
+    return timer;
+}
+
+
+std::mutex &GallocyState::get_timer_mutex() {
+    return timed_out_mutex;
+}
+
+
+std::condition_variable &GallocyState::get_timer_cv() {
+    return timed_out;
+}
+
+
+RaftState GallocyState::get_state() {
+    std::lock_guard<std::mutex> lock(access_lock);
+    return state;
+}
+
+
+void GallocyState::set_state(RaftState new_state) {
+    std::lock_guard<std::mutex> lock(access_lock);
+
+    // Only log state changes.
+    if (new_state != state)
+        LOG_INFO("Changing state from "
+                << L_ORANGE(raft_state_to_string(state))
+                << " to "
+                << L_ORANGE(raft_state_to_string(new_state))
+                << " in "
+                << "("
+                << "term=" << current_term << ", "
+                << "l.a.=" << last_applied << ", "
+                << "c.i.=" << commit_index
+                << ")");
+
+    if (new_state == RaftState::LEADER) {
+        timer->set_step(LEADER_STEP_TIME);
+        timer->set_jitter(LEADER_JITTER_TIME);
+        // TODO(sholsapp): Initialize the leader here for each peer.
+    } else {
+        timer->set_step(FOLLOWER_STEP_TIME);
+        timer->set_jitter(FOLLOWER_JITTER_TIME);
+    }
+    state = new_state;
 }
--- gallocy/include/gallocy/consensus/state.h
+++ gallocy/include/gallocy/consensus/state.h
@@ -74,101 +74,81 @@ class GallocyState {
   /**
    * Get the current term.
    */
-  uint64_t get_current_term() {
-    std::lock_guard<std::mutex> lock(access_lock);
-    return current_term;
-  }
+  uint64_t get_current_term();
   /**
    * Set the current term.
    *
    * In setting a new current term, the state that tracks the candidate we
    * last voted for is implicitly reset.
    */
-  void set_current_term(uint64_t value) {
-    std::lock_guard<std::mutex> lock(access_lock);
-    if (value < current_term)
-      LOG_ERROR("Regressing current term from "
-          << current_term
-          << " to "
-          << value
-          << ". This is a logic error.");
-    current_term = value;
-    voted_for = 0;
-  }
+  void set_current_term(uint64_t value);
   /**
    * Get candidate that received vote in current term.
    */
-  PeerId get_voted_for() {
-    std::lock_guard<std::mutex> lock(access_lock);
-    return voted_for;
-  }
+  PeerId get_voted_for();
   /**
    * Set candidate that received vote in current term.
    */
-  void set_voted_for(PeerId value) {
-    std::lock_guard<std::mutex> lock(access_lock);
-    voted_for = value;
-  }
+  void set_voted_for(PeerId value);
   /**
    * Get the commit index.
    */
-  uint64_t get_commit_index() {
-    std::lock_guard<std::mutex> lock(access_lock);
-    return commit_index;
-  }
+  uint64_t get_commit_index();
   /**
    * Set the commit index.
    */
-  void set_commit_index(uint64_t value) {
-    std::lock_guard<std::mutex> lock(access_lock);
-    commit_index = value;
-  }
+  void set_commit_index(uint64_t value);
   /**
    * Get the index of the last applied log entry.
    */
-  uint64_t get_last_applied() {
-    std::lock_guard<std::mutex> lock(access_lock);
-    return last_applied;
-  }
+  uint64_t get_last_applied();
   /**
    * Set the index of the last applied log entry.
    */
-  void set_last_applied(uint64_t value) {
-    std::lock_guard<std::mutex> lock(access_lock);
-    last_applied = value;
-  }
+  void set_last_applied(uint64_t value);
   /**
    * Get index of the next log entry to send to peer.
    */
-  uint64_t get_next_index(PeerId peer) {
-    std::lock_guard<std::mutex> lock(access_lock);
-    return next_index[peer];
-  }
+  uint64_t get_next_index(PeerId peer);
   /**
    * Set index of the next log entry to send to peer.
    */
-  void set_next_index(PeerId peer, uint64_t value) {
-    std::lock_guard<std::mutex> lock(access_lock);
-    next_index[peer] = value;
-  }
+  void set_next_index(PeerId peer, uint64_t value);
   /**
    * Get index of highest log entry known to be replicated on peer.
    */
-  uint64_t get_match_index(PeerId peer) {
-    std::lock_guard<std::mutex> lock(access_lock);
-    return match_index[peer];
-  }
+  uint64_t get_match_index(PeerId peer);
   /**
    * Set index of highest log entry known to be replicated on peer.
    */
-  void set_match_index(PeerId peer, uint64_t value) {
-    std::lock_guard<std::mutex> lock(access_lock);
-    match_index[peer] = value;
-  }
+  void set_match_index(PeerId peer, uint64_t value);
+  /**
+   * Get the state machine log.
+   */
+  GallocyLog *get_log();
+  /**
+   * Get the timer.
+   */
+  Timer *get_timer();
+  /**
+   * Get the timer's mutex.
+   */
+  std::mutex &get_timer_mutex();
+  /**
+   * Get the timer condition variable.
+   *
+   * To get this timer's mutex, use :meth:`get_timer_mutex`.
+   */
+  std::condition_variable &get_timer_cv();
+  /**
+   * Get the current state of this node.
+   */
+  RaftState get_state();
+  /**
+   * Set the current state of this node.
+   */
+  void set_state(RaftState new_state);
 
-  //
-  // Persistent state on all servers.
-  //
  private:
   /**
    * The latest term this server has seen.
@@ -185,11 +165,6 @@ class GallocyState {
    * entry was received by the leader.
    */
   GallocyLog *log;
-
-  //
-  // Volatile state on all servers.
-  //
- private:
   /**
    * Index of highest log entry known to be committed.
    */
@@ -198,11 +173,6 @@ class GallocyState {
    * Index of highest log entry applied to state machine.
    */
   uint64_t last_applied;
-
-  //
-  // Volatile state on leaders.
-  //
- private:
   /**
    * Mapping of peer to next log entry to send to the peer.
    */
@@ -211,13 +181,6 @@ class GallocyState {
    * Mapping of peer to highest log entry known to be replicated on the peer.
    */
   gallocy::map<PeerId, uint64_t> match_index;
-
-  //
-  // Internal implementation details.
-  //
- private:
-  GallocyConfig &config;
-  RaftState state;
   /**
    * A lock to synchronize all get/set access to private member data.
    */
@@ -233,70 +196,8 @@ class GallocyState {
   Timer *timer;
   std::condition_variable timed_out;
   std::mutex timed_out_mutex;
-
- public:
-  /**
-   * Get the state machine log.
-   */
-  GallocyLog *get_log() {
-    return log;
-  }
-  /**
-   * Get the timer.
-   */
-  Timer *get_timer() {
-    return timer;
-  }
-  /**
-   * Get the timer's mutex.
-   */
-  std::mutex &get_timer_mutex() {
-    return timed_out_mutex;
-  }
-  /**
-   * Get the timer condition variable.
-   *
-   * To get this timer's mutex, use :meth:`get_timer_mutex`.
-   */
-  std::condition_variable &get_timer_cv() {
-    return timed_out;
-  }
-  /**
-   * Get the current state of this node.
-   */
-  RaftState get_state() {
-    std::lock_guard<std::mutex> lock(access_lock);
-    return state;
-  }
-  /**
-   * Set the current state of this node.
-   */
-  void set_state(RaftState new_state) {
-    std::lock_guard<std::mutex> lock(access_lock);
-
-    // Only log state changes.
-    if (new_state != state)
-      LOG_INFO("Changing state from "
-          << L_ORANGE(raft_state_to_string(state))
-          << " to "
-          << L_ORANGE(raft_state_to_string(new_state))
-          << " in "
-          << "("
-          << "term=" << current_term << ", "
-          << "l.a.=" << last_applied << ", "
-          << "c.i.=" << commit_index
-          << ")");
-
-    if (new_state == RaftState::LEADER) {
-      timer->set_step(LEADER_STEP_TIME);
-      timer->set_jitter(LEADER_JITTER_TIME);
-      // TODO(sholsapp): Initialize the leader here for each peer.
-    } else {
-      timer->set_step(FOLLOWER_STEP_TIME);
-      timer->set_jitter(FOLLOWER_JITTER_TIME);
-    }
-    state = new_state;
-  }
+  GallocyConfig &config;
+  RaftState state;
 };
 
 #endif  // GALLOCY_CONSENSUS_STATE_H_
